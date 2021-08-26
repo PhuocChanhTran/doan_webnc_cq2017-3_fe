a09087006645d77515226f5715ae1dcbad4c722d
PhuocChanhTran <46354066+PhuocChanhTran@users.noreply.github.com>
2021-08-27 02:42:24 +0700

    Multiple Menu

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -9,21 +9,44 @@ import {
   Navbar,
   NavDropdown,
 } from "react-bootstrap";
+import { getAllSubject, getAllCat } from "../services/header.service";
+
+import Dropdown from "react-multilevel-dropdown";
 import { useContext, useEffect, useState } from "react";
 
 import "../App.css";
 import FrameCart from "./Cart/FrameCart";
 import FrameWatch from "./WatchList/FrameWatch";
-import {AppContext} from "../contexts/AppProvider";
-import logo from '../assets/images/logo.png'
-import pageBg1 from '../assets/images/page-bg/1.jpg'
+import { AppContext } from "../contexts/AppProvider";
+import logo from "../assets/images/logo.png";
+import pageBg1 from "../assets/images/page-bg/1.jpg";
 export default function Header(props) {
   const [searchKey, setsearchKey] = useState("");
-  const [categoryFilter,setCategoryFilter] = useState("default");
+  const [categoryFilter, setCategoryFilter] = useState("default");
   const history = useHistory();
-  const {store} =useContext(AppContext);
-  
-  
+  const { store } = useContext(AppContext);
+  const [subject, setSubject] = useState([]);
+  const [cat, setCat] = useState([]);
+  useEffect(function () {
+    async function loadAllSubject() {
+      const res = await getAllSubject();
+      console.log("allsubject laf");
+      console.log(res.data);
+      console.log("allsubject laf");
+      setSubject(res.data);
+    }
+    async function loadAllCat() {
+      const res = await getAllCat();
+      console.log("allsubject laf");
+      console.log(res.data);
+      console.log("allsubject laf");
+      setCat(res.data);
+    }
+
+    loadAllSubject();
+    loadAllCat();
+  }, []);
+
   const signOutBtn_Clicked = () => {
     delete localStorage.userEmail;
     delete localStorage.accessToken;
@@ -35,19 +58,21 @@ export default function Header(props) {
   };
   const handleButton_click = function () {
     console.log(categoryFilter);
-    history.push(`/courses/search?course=${searchKey}&categoryId=${categoryFilter}`);
+    history.push(
+      `/courses/search?course=${searchKey}&categoryId=${categoryFilter}`
+    );
   };
-  const handleFilterByCategory = (e)=> {
+  const Cat_Click = function (catId) {
+    history.push(`/courses/category/${catId}`);
+  };
+  const handleFilterByCategory = (e) => {
     console.log(e.target.value);
     setCategoryFilter(e.target.value);
-  }
+  };
 
   return (
     <div>
-      <div
-        className=" set-bg top-container"
-        data-setbg={pageBg1}
-      >
+      <div className=" set-bg top-container" data-setbg={pageBg1}>
         <nav class="secondary-menu">
           <div>
             <div class="sm-left">
@@ -105,7 +130,10 @@ export default function Header(props) {
                   aria-label="Search"
                   onChange={handleSearchKeyChanged}
                 />
-                <FormSelect onChange={(e)=>handleFilterByCategory(e)} defaultValue="default">
+                <FormSelect
+                  onChange={(e) => handleFilterByCategory(e)}
+                  defaultValue="default"
+                >
                   <option value="default">All</option>
                   <option value={1}>Web Course</option>
                   <option value={2}>Mobile Course</option>
@@ -117,28 +145,40 @@ export default function Header(props) {
             </div>
             <div className="col-sm navbar-custom">
               <Nav className="">
-                <NavDropdown title="IT" id="basic-nav-dropdown">
+                {/* <NavDropdown title="IT" id="basic-nav-dropdown">
                   <NavDropdown.Item>
-                    <Link
-                      to="/courses/category/1"
-                      style={{ color: "#000" }}
-                    >
+                    <Link to="/courses/category/1" style={{ color: "#000" }}>
                       Web courses
                     </Link>
                   </NavDropdown.Item>
                   <NavDropdown.Item>
-                    <Link
-                      to="/courses/category/2"
-                      style={{ color: "#000" }}
-                    >
-
+                    <Link to="/courses/category/2" style={{ color: "#000" }}>
                       Mobile courses
                     </Link>
                   </NavDropdown.Item>
-                </NavDropdown>
+                </NavDropdown> */}
+                <Dropdown title="Category" position="right">
+                  {subject.map((subject) => (
+                    <Dropdown.Item position="right" className="text-primary">
+                      {subject.name}
+                      <Dropdown.Submenu position="right">
+                        {cat.map((cat) =>
+                          cat.subject_id === subject.subject_id ? (
+                            <Dropdown.Item
+                              onClick={() => Cat_Click(cat.category_id)}
+                            >
+                              {cat.category_name}
+                            </Dropdown.Item>
+                          ) : (
+                            ""
+                          )
+                        )}
+                      </Dropdown.Submenu>
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown>
                 {localStorage.userEmail ? (
                   <>
-
                     <Nav.Link className="cart-nav">
                       <Link to="#">
                         <i class="fa fa-shopping-cart" aria-hidden="true"></i>{" "}
@@ -156,7 +196,6 @@ export default function Header(props) {
                       <Link to="/checkout">
                         <i class="fa fa-shopping-cart" aria-hidden="true"></i>{" "}
                         Checkout
-
                       </Link>
                     </Nav.Link>
                     <NavDropdown
@@ -164,7 +203,7 @@ export default function Header(props) {
                       id="basic-nav-dropdown"
                     >
                       <NavDropdown.Item>
-                      <Link to="/settings" style={{ color: "#000" }}>
+                        <Link to="/settings" style={{ color: "#000" }}>
                           Settings
                         </Link>
                       </NavDropdown.Item>
@@ -176,19 +215,18 @@ export default function Header(props) {
                       {localStorage.userRole === "lecturer" ? (
                         <>
                           <NavDropdown.Item>
-                          <Link to="/mycourses" style={{ color: "#000" }}>
-                            My courses
-                          </Link>
-                        </NavDropdown.Item>
-                        <NavDropdown.Item>
-                        <Link
-                          to="/mysubcribecourses"
-                          style={{ color: "#000" }}
-                        >
-
-                          My subscribe courses
-                        </Link>
-                      </NavDropdown.Item>
+                            <Link to="/mycourses" style={{ color: "#000" }}>
+                              My courses
+                            </Link>
+                          </NavDropdown.Item>
+                          <NavDropdown.Item>
+                            <Link
+                              to="/mysubcribecourses"
+                              style={{ color: "#000" }}
+                            >
+                              My subscribe courses
+                            </Link>
+                          </NavDropdown.Item>
                         </>
                       ) : (
                         <NavDropdown.Item>
@@ -196,7 +234,6 @@ export default function Header(props) {
                             to="/mysubcribecourses"
                             style={{ color: "#000" }}
                           >
-
                             My subscribe courses
                           </Link>
                         </NavDropdown.Item>
@@ -207,7 +244,9 @@ export default function Header(props) {
                     </NavDropdown>
                   </>
                 ) : (
-                  <Nav.Link ><Link to="/login">Login</Link></Nav.Link>
+                  <Nav.Link>
+                    <Link to="/login">Login</Link>
+                  </Nav.Link>
                 )}
               </Nav>
             </div>
